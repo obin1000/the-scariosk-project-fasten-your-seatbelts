99e4c5f3233edbd2ed706cb43b3b1de266b374bf
R. Vonk <robin.vonk@hva.nl>
2017-11-29 16:07:13 +0100

    Toevoeging van andere onderdelen

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from camera import foto
-from button import button
+#from camera import foto
+#from button import button
+from database import database
 
 if __name__ == "__main__":
-    knop = button()
-    maakFoto = foto()
-    maakFoto.saveResolutie(1920,1080)
-    
-    while True:
-        if knop.triggerButton():
-            maakFoto.takePictures()
+    base = database()
+    if base.checkCode(69):
+        print('code vrij')
+    else:
+        print('code niet vrij')
+    #base.insertData('69','lol/lal')
+    #knop = button()
+    #maakFoto = foto()
+    #maakFoto.saveResolutie(1920,1080)
+    #maakFoto.savePath('/home/pi/Documents/')
+    #while True:
+    #    if knop.triggerButton():
+    #        maakFoto.takePictures()
     
     
