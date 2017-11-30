8f66e3a38a9776f52f16f4f868565c5e8b7a4d13
R.M. Blom <rutger.blom2@hva.nl>
2017-11-30 21:09:08 +0100

    Update Gui.py

---
+++ Gui.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5 import QtWidgets, QtGui
+
+class Window(QtWidgets.QWidget):
+
+	def __init__(self):
+		super().__init__()
+
+		self.init_ui()
+
+	def init_ui(self):
+		self.b = QtWidgets.QPushButton('Take photo')
+		self.b1 = QtWidgets.QPushButton('New photo')
+		self.b1.hide()
+		self.l = QtWidgets.QLabel('Live feed')
+
+		h_box = QtWidgets.QHBoxLayout()
+		h_box.addStretch()
+		h_box.addWidget(self.l)
+		h_box.addStretch()
+
+		v_box = QtWidgets.QVBoxLayout()
+		v_box.addWidget(self.b)
+		v_box.addWidget(self.b1)
+		v_box.addLayout(h_box)
+
+		self.setLayout(v_box)
+		self.setWindowTitle('PyQt Lesson 5')
+		self.setGeometry(150, 100, 350, 450)
+
+		self.b.clicked.connect(self.btn_click)
+		self.b1.clicked.connect(self.btn_click1)
+
+		self.show()
+
+	def btn_click(self):
+		self.l.setPixmap(QtGui.QPixmap('Test.png'))
+		self.b.hide()
+		self.b1.show()
+
+	def btn_click1(self):
+		self.l.setText('Take new photo')
+		self.b1.hide()
+		self.b.show()
+
+app = QtWidgets.QApplication(sys.argv)
+a_window = Window()
+sys.exit(app.exec_())
