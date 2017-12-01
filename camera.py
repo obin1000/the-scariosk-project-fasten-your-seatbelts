8b8273f3035db3cbbeb311081af0f9f21c6d141f
R. Vonk <robin.vonk@hva.nl>
2017-12-01 08:55:53 +0100

    opruimen

--- camera.py
+++ camera.py
@@ -1,4 +1,4 @@
-import picamera #import benodigde libraries
+import picamera
 class foto:
     
     def saveResolutie(self, horizontaal, verticaal):
@@ -7,14 +7,13 @@ class foto:
         
         
     def takePictures(self):
-        camera = picamera.PiCamera() #variable voor de camera
+        camera = picamera.PiCamera()
         camera.resolution = (self.pixelsH, self.pixelsV)
         #camera.hflip = rotatieHorizontaal
         #camera.vflip = rotatieVerticaal
         print(self.pixelsH + self.pixelsV)
         #loop voor het maken van 6 burst fotos
         for count in range (0 , 6):
-            #maak de foto en sla hem op volgens het pad
             camera.capture('/home/pi/Downloads/'+str(count+1)+'.jpg')
             #print dat de foto genomen is
             print('picture ' + str(count+1) + ' taken!')
