e956e097b88ae0334b9eec67825d45a61ca2c559
R. Vonk <robin.vonk@hva.nl>
2017-12-07 22:17:43 +0100

    Toevoeging van de flits

--- gpio.py
+++ gpio.py
@@ -3,15 +3,22 @@ import time
 
 class gpio:
     
-    GPIO.setmode(GPIO.BCM)                              #Gebruik de BCM nummering van de raspberrypi pins 
-    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Set pin 12 als een input en enable de ingebouwde weerstand om kortsluitng te voorkomen
-    GPIO.add_event_detect(18, GPIO.RISING)              #Aanmaak detector voor response op pin 18
+    GPIO.setmode(GPIO.BOARD)                            #Gebruik de BOARD nummering van de raspberrypi pins 
+    GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Set pin 18 als een input en enable de ingebouwde weerstand om kortsluitng te voorkomen
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.add_event_detect(12, GPIO.RISING)              #Aanmaak detector voor response op pin 18
+
     
     def triggerButton(self):
-        if GPIO.event_detected(18):
+        if GPIO.event_detected(12):
             return True
         
-    #def flits(self):
+    def flits(self,duur):
+        GPIO.output(18,1)
+        time.sleep(duur)
+        GPIO.output(18,0)
+        
+
         
         
 
