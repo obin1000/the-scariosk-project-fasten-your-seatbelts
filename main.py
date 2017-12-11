c7ef1cb29f21d95ed0d8d4b26436ac84a8220fb5
R. Vonk <robin.vonk@hva.nl>
2017-12-11 15:35:32 +0100

    Toevoeging van de verbinding met de database

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 #Imports geleend van andere mensen
 import time
+#from threading import Thread
 
 #Imports zelf gemaakt
 from camera import foto
 from gpio import gpio
-#from database import database
-#from randomCode import randomCode
+from database import database
+from randomCode import randomCode
 
 if __name__ == "__main__":
-
-    #code = randomCode()
-    #print(code.generateCode())
-    #base = database()
-    knop = gpio()
-    maakFoto = foto()
-    #base.insertData('69','lol/lal')
-
-    maakFoto.savePath('/home/pi/Documents/')
-    maakFoto.saveResolutie(1920,1080)
-    maakFoto.saveRotation(True, True)
-    maakFoto.setupCamera()
-    while True:
-        if knop.triggerButton():
-            maakFoto.takePictures(1345,6)
-            time.sleep(1)  
     
+    path = '/home/pi/Documents/'
+    base = database()
+    iobord = gpio()
+    maakFoto = foto()
+    
+    maakFoto.savePath(path)
+    maakFoto.saveResolutie(1920,1080)
+    maakFoto.saveRotation(False,False)
+    maakFoto.setupCamera()
+    
+    while True:
+        if iobord.triggerButton():
+            code = randomCode().generateCode()
+            maakFoto.takePictures(code,6)
+            base.insertData(code,(path + str(code)))
+        time.sleep(1)
+    #Thread(target = ).start()
