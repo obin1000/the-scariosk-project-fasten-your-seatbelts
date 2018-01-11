8ee4383a6555ed0b5bac88c70508111a63fcc710
M. El Hadiyen <mohamed.el.hadiyen@hva.nl>
2018-01-11 14:21:04 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -9,23 +9,29 @@ windows.configure(background="white")
 windows.attributes('-fullscreen', True)
 windows.bind("q", quit)
     
-def countDown3(self):
+def countdown3(self):
     drie = Canvas(windows,highlightthickness = 0, width = 800, height = 400)
     drie.pack()
     drie.configure(background = "white")
     drie.create_text(350, 125, anchor = N, font = "Helvetica 70", fill = "darkred", text = "3....")
     
-def countDown2(self):
+def countdown2(self):
     twee = Canvas(windows, highlightthickness = 0, width = 800, height = 400)
     twee.pack()
     twee.configure(background="white")
     twee.create_text(350, 125, achter = N, font = "Helvetica 70", fill = "darkred", text = "2....")
     
-def countDown1(self):
+def countdown1(self):
     een = Canvas(windows, highlightthickness = 0, width = 800, height = 400)
     een.pack()
     een.configure(background="white")
     een.create_text(350, 125, achter = N, font = "Helvetica 70", fill = "darkred", text = "1....")
+    
+def countdownGo(self):
+    een = Canvas(windows, highlightthickness = 0, width = 800, height = 400)
+    een.pack()
+    een.configure(background="white")
+    een.create_text(350, 125, achter = N, font = "Helvetica 70", fill = "darkred", text = "Smile!!")
 
 def beginDia(self):
     tekst = Canvas(windows,highlightthickness = 0, width = 800, height = 30)
