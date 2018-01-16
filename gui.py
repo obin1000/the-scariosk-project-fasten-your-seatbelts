54a7213da7b0ba2eb6ff4ff071456dc4ee548928
R. Vonk <robin.vonk@hva.nl>
2018-01-16 16:22:15 +0100

    Replace gui.py

--- gui.py
+++ gui.py
@@ -1,29 +1,38 @@
 from tkinter import *
 import time
 
-class gui:
 
-	windows = Tk()
-	windows.title('Scariosk')
-	windows.geometry("800x400")
-	windows.configure(background="black")
-	windows.attributes('-fullscreen', True)
-	windows.bind("q", quit)
+def quit(self):
+    windows.destroy()
 
-	corendon = PhotoImage(file = 'scariosk.gif')
-	test = PhotoImage(file = 'test.gif')
-	testok = Label(windows, image=corendon)
-	testok.place(x=0,y=0)
-	tello = Label(windows, image=test)
+def selectFrame(frame):
+    frame.tkraise()
 
-	def quit(self):
-		windows.destroy()
+def updateScherm():
+    while True:
+        windows.update_idletasks()
+        windows.update()
 
+windows = Tk()
+windows.title('Scariosk')
+windows.geometry("800x480")
+windows.configure(background="black")
+windows.attributes('-fullscreen', True)
+windows.bind("q", quit)
+windows.bind("1", selectFrame(f2))
 
-	def change(self):
-		testok.place_forget()
-		tello.place(x=0,y=0)
+f1 = Frame(windows)
+f2 = Frame(windows)
+f3 = Frame(windows)
 
-	windows.bind("1", change)
+for frame in (f1, f2, f3):
+    frame.grid(row=0, column=0, sticky='news')
+    
+testImage = PhotoImage(file = 'media/test.gif')
+buttonImage = PhotoImage(file = 'media/scariosk.gif')
+Label(f1, image=buttonImage).pack()
+Label(f2, image=testImage).pack()
+selectFrame(f1)
 
-##windows.mainloop()
+windows.mainloop()
+
