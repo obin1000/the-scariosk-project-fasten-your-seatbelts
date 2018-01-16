32296bb5fdea5a82d43457f16fe6bc164552f516
M. El Hadiyen <mohamed.el.hadiyen@hva.nl>
2018-01-16 02:28:17 +0100

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -14,32 +14,35 @@ windows.bind("q", quit)
 def quit(self):
     windows.destroy()
 
-frameBegin = Frame(windows)
-canvas = Canvas(frameBegin,highlightthickness = 0, width = 800, height = 400)
-canvas.pack
 corendon = PhotoImage(file = 'scariosk.gif')
 test = PhotoImage(file = 'test.gif')
 ##canvas.create_image(0, 0, image = corendon, anchor = NW)
-frameBegin.grid()
 test = Label(windows, image=corendon)
 test.place(x=0,y=0)
 test1 = Label(windows, image=test)
+test2 = Label(windows, image=test1)
+test3 =
+test4 =
 
 def change(self):
 	test.place_forget()
 	test1.place(x=0,y=0)
 	
 def change2(self):
-	test.place_forget()
-	test1.place(x=0,y=0)
+	test1.place_forget()
+	test2.place(x=0,y=0)
 	
 def change3(self):
-	test.place_forget()
-	test1.place(x=0,y=0)
+	test2.place_forget()
+	test3.place(x=0,y=0)
+
+def change4(self):
+    test3.place_forget()
+    test4.place(x=0,y=0)
 	
 def repeat(self):
-	test.place_forget()
-	test1.place(x=0,y=0)
+	test4.place_forget()
+	test.place(x=0,y=0)
 
 windows.bind("1", change)
 
